c21c925a66bb20b558cbc9131976fd38fca934ea
Quan <qhoang2373@gmail.com>
2024-07-28 16:29:44 -0700

    Consts Completed

---
+++ app.js
@@ -0,0 +1,22 @@
+// /*-------------------------------- Constants --------------------------------*/
+const calculator = document.getElementById('calculator');
+const display = calculator.querySelector('.display');
+const buttons = calculator.querySelectorAll('.button');
+
+
+// /*-------------------------------- Variables --------------------------------*/
+
+
+
+
+// /*------------------------ Cached Element References ------------------------*/
+
+
+// /*----------------------------- Event Listeners -----------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+
